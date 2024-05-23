fbadf024ca1ac1e26be1ebe7f9ef34bbe53bdaaa
macluiggy <luiggy.macias@imagineapps.co>
2024-05-22 20:44:30 -0500

    chore: Add 'users' app and API endpoints for user management

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls'))
+    path('', include('myapp.urls')),
+    path('api/v1/', include('users.urls')),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Users
+from .serializers import UsersSerializer
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+    
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+        data['password'] = make_password(data['password'])  # Hash the password
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    @action(detail=False, methods=['get'])
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
+    
