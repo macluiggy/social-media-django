f389338c781a44dbdb422dac5acafccbcb6e1307
macluiggy <luiggy.macias@imagineapps.co>
2024-05-22 20:32:30 -0500

    chore: Refactor views.py to improve code structure and remove unused code

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,23 +1,14 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
 from .models import todoItem
-# impot json response
 from django.http import JsonResponse
 
 
 # Create your views here.
 def home(request):
     return HttpResponse("Hello, World!")
-    # return render(request, "home.html")
 
 
 def todos(request):
     items = todoItem.objects.all()
     return JsonResponse(list(items.values()), safe=False)
-    # return HttpResponse('items')
-    # return render(
-    #     request,
-    #     "todos.html",
-    #     {
-    #         "todos": items,
-    #     },
-    # )
