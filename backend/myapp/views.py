f6acf11b961d477bdfe8f08f33e5155bbbbae2b4
macluiggy <luiggy.macias@imagineapps.co>
2024-05-22 19:37:20 -0500

    chore: Add myapp to INSTALLED_APPS and update URL configuration

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render, HttpResponse
+from .models import todoItem
 
 
 # Create your views here.
 def home(request):
     # return HttpResponse("Hello, World!")
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
+
+def todos(request):
+    items = todoItem.objects.all()
+    # return HttpResponse('items')
+    return render(
+        request,
+        "todos.html",
+        {
+            "todos": items,
+        },
+    )
