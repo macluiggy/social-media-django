f46aa3d67792e17c0af036b10536d88fa5e1a85b
macluiggy <luiggy.macias@imagineapps.co>
2024-05-23 21:14:59 -0500

    feat: Improve login endpoint with user authentication and token generation

--- temp/backend/backend/view.py
+++ temp/backend/backend/view.py
@@ -4,10 +4,18 @@ from .serializers import UserSerializer
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 @api_view(['POST'])
 def login(request):
-    return Response({'token': '1234'})
+    user = get_object_or_404(User, username=request.data['username'])
+    
+    if not user.check_password(request.data['password']):
+        return Response({'error': 'Invalid password'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+    return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def register(request):
